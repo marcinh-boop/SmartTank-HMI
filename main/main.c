3d8518950f81e8527b2b25a0f4cf338618239383
Marcin <marcin@example.com>
2026-07-09 23:18:21 +0200

    Uruchomienie LCD LVGL PSRAM

--- main/main.c
+++ main/main.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include "waveshare_rgb_lcd_port.h"
+#include "lvgl_port.h"
+
+void example_lvgl_demo_ui(void);
 
 void app_main(void)
 {
+    waveshare_esp32_s3_rgb_lcd_init();
 
+    if (lvgl_port_lock(-1)) {
+        example_lvgl_demo_ui();
+        lvgl_port_unlock();
+    }
 }
