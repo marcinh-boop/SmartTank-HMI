b493554caf9eeda0ee1568c70da880a0028a4aa0
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 18:00:07 +0200

    Start minimal display diagnostic instead of HMI

--- main/app/app.c
+++ main/app/app.c
@@ -1,13 +1,11 @@
 #include "app.h"
 #include "lvgl_port.h"
-#include "theme.h"
-#include "screen_dashboard.h"
+#include "screen_display_test.h"
 
 void app_start(void)
 {
     if (lvgl_port_lock(-1)) {
-        theme_init();
-        screen_dashboard_create();
+        screen_display_test_create();
         lvgl_port_unlock();
     }
 }
