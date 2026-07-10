623f56d313201c3b9b15700b2a8e4b8071bfa735
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 20:37:45 +0200

    Start SmartTank model and simulator

--- main/app/app.c
+++ main/app/app.c
@@ -1,9 +1,17 @@
 #include "app.h"
+
+#include "esp_err.h"
+
+#include "app_model.h"
+#include "data_simulator.h"
 #include "lvgl_port.h"
 #include "screen_dashboard.h"
 
 void app_start(void)
 {
+    ESP_ERROR_CHECK(app_model_init());
+    ESP_ERROR_CHECK(data_simulator_start());
+
     if (lvgl_port_lock(-1)) {
         screen_dashboard_create();
         lvgl_port_unlock();
