aa993d033292293c9e4d4837645ae9bbb92ddafd
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 16:58:39 +0200

    Make bottom navigation instance-safe

--- main/widgets/bottom_nav.h
+++ main/widgets/bottom_nav.h
@@ -15,14 +15,23 @@ typedef enum {
 
 typedef bool (*bottom_nav_change_cb_t)(bottom_nav_page_t page);
 
+typedef struct bottom_nav bottom_nav_t;
+
 typedef struct {
+    bottom_nav_t *nav;
+    bottom_nav_page_t page;
+} bottom_nav_button_ctx_t;
+
+struct bottom_nav {
     lv_obj_t *root;
     lv_obj_t *buttons[NAV_ITEM_COUNT];
     lv_obj_t *labels[NAV_ITEM_COUNT];
+    bottom_nav_button_ctx_t contexts[NAV_ITEM_COUNT];
     bottom_nav_page_t active_page;
-} bottom_nav_t;
+    bottom_nav_change_cb_t change_cb;
+};
 
-bottom_nav_t bottom_nav_create(
+bottom_nav_t *bottom_nav_create(
     lv_obj_t *parent,
     bottom_nav_page_t active_page,
     bottom_nav_change_cb_t change_cb
