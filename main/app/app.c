492bab4cb064f035abefcdcbe774f3b515b4870b
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 20:49:19 +0200

    Initialize measurement history before simulator

--- main/app/app.c
+++ main/app/app.c
@@ -3,6 +3,7 @@
 #include "esp_err.h"
 
 #include "app_model.h"
+#include "measurement_history.h"
 #include "data_simulator.h"
 #include "lvgl_port.h"
 #include "screen_dashboard.h"
@@ -10,6 +11,7 @@
 void app_start(void)
 {
     ESP_ERROR_CHECK(app_model_init());
+    ESP_ERROR_CHECK(measurement_history_init());
     ESP_ERROR_CHECK(data_simulator_start());
 
     if (lvgl_port_lock(-1)) {
