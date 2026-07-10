2e33b47ba64a64559e5525984808499e3362aa73
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 22:24:46 +0200

    Add PCF85063 RTC driver interface

---
+++ main/drivers/rtc_pcf85063.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "driver/i2c.h"
+#include "esp_err.h"
+
+#define SMARTTANK_RTC_I2C_PORT    I2C_NUM_0
+#define SMARTTANK_RTC_I2C_ADDRESS 0x51U
+
+typedef struct {
+    uint16_t year;
+    uint8_t month;
+    uint8_t day;
+    uint8_t weekday;
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+    bool oscillator_stopped;
+} rtc_datetime_t;
+
+esp_err_t rtc_pcf85063_probe(void);
+esp_err_t rtc_pcf85063_read(rtc_datetime_t *datetime);
+esp_err_t rtc_pcf85063_write(const rtc_datetime_t *datetime);
+bool rtc_pcf85063_datetime_is_valid(const rtc_datetime_t *datetime);
