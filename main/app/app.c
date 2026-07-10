a16a4ca06d1325c9ad65c830e1813ce7b29b3df5
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 17:09:08 +0200

    Initialize only the persistent main screen

--- main/app/app.c
+++ main/app/app.c
@@ -2,20 +2,12 @@
 #include "lvgl_port.h"
 #include "theme.h"
 #include "screen_dashboard.h"
-#include "screen_history.h"
 
 void app_start(void)
 {
     if (lvgl_port_lock(-1)) {
         theme_init();
-
-        /*
-         * Budujemy oba ekrany podczas startu, zanim LVGL narysuje pierwsza klatke.
-         * Pozniejsze przelaczanie tylko podmienia wskaznik aktywnego ekranu.
-         */
-        screen_history_create();
         screen_dashboard_create();
-
         lvgl_port_unlock();
     }
 }
