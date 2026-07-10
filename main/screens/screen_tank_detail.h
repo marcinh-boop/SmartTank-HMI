07600cb154062d1fca4680484be0ba7386acd68d
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 20:57:12 +0200

    Add tank detail screen interface

---
+++ main/screens/screen_tank_detail.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "app_model.h"
+#include "lvgl.h"
+
+typedef void (*screen_tank_detail_back_cb_t)(void);
+
+lv_obj_t *screen_tank_detail_create(
+    lv_obj_t *parent,
+    screen_tank_detail_back_cb_t back_cb
+);
+
+void screen_tank_detail_update(const smarttank_state_t *state);
