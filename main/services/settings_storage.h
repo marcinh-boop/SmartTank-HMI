ffa9855d7c06fe29a6aca59fcf4099153d41354a
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 22:55:01 +0200

    Add persisted Wi-Fi credentials API

--- main/services/settings_storage.h
+++ main/services/settings_storage.h
@@ -3,6 +3,17 @@
 #include "app_model.h"
 #include "esp_err.h"
 
+#define SETTINGS_WIFI_SSID_MAX_LEN      32U
+#define SETTINGS_WIFI_PASSWORD_MAX_LEN  64U
+
+typedef struct {
+    char ssid[SETTINGS_WIFI_SSID_MAX_LEN + 1U];
+    char password[SETTINGS_WIFI_PASSWORD_MAX_LEN + 1U];
+} wifi_credentials_t;
+
 esp_err_t settings_storage_init(void);
 esp_err_t settings_storage_load_tank_config(tank_channel_config_t *config);
 esp_err_t settings_storage_save_tank_config(const tank_channel_config_t *config);
+esp_err_t settings_storage_load_wifi_credentials(wifi_credentials_t *credentials);
+esp_err_t settings_storage_save_wifi_credentials(const wifi_credentials_t *credentials);
+esp_err_t settings_storage_clear_wifi_credentials(void);
