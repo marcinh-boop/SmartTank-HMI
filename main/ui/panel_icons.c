da677c4f564cabd36094ab713c7ccefdea6416e0
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-11 00:30:52 +0200

    Popraw pozycjonowanie ikony pogody

--- main/ui/panel_icons.c
+++ main/ui/panel_icons.c
@@ -146,10 +146,10 @@ static void create_large_weather_icon(lv_obj_t *card)
     hide_weather_placeholders(card);
 
     lv_obj_t *root = lv_obj_create(card);
-    lv_obj_set_size(root, 82, 82);
-    lv_obj_align(root, LV_ALIGN_TOP_LEFT, -2, 72);
-    make_noninteractive(root);
     lv_obj_remove_style_all(root);
+    lv_obj_set_size(root, 82, 82);
+    lv_obj_align(root, LV_ALIGN_TOP_LEFT, -2, 52);
+    make_noninteractive(root);
 
     create_solid_shape(root, 25, 25, 43, 3, ICON_WEATHER_COLOR, LV_RADIUS_CIRCLE);
     create_solid_shape(root, 3, 8, 54, 0, ICON_WEATHER_COLOR, 1);
