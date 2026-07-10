883a1127b0c2433acf714303cc0976cd0c4ddcdf
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 19:06:49 +0200

    Use Waveshare double-buffer full-refresh mode

--- main/lvgl_port.h
+++ main/lvgl_port.h
@@ -20,39 +20,21 @@ extern "C" {
 #define ESP_PANEL_USE_1024_600_LCD           (0)     // 0: 800x480, 1: 1024x600
 #define CONFIG_EXAMPLE_LCD_TOUCH_CONTROLLER_GT911 1 // 1 initiates the touch, 0 closes the touch.
 
-/**
- * LVGL related parameters, can be adjusted by users
- *
- */
 #if ESP_PANEL_USE_1024_600_LCD
-    #define LVGL_PORT_H_RES             (1024)
-    #define LVGL_PORT_V_RES             (600)
+#define LVGL_PORT_H_RES             (1024)
+#define LVGL_PORT_V_RES             (600)
 #else
-    #define LVGL_PORT_H_RES             (800)
-    #define LVGL_PORT_V_RES             (480)
+#define LVGL_PORT_H_RES             (800)
+#define LVGL_PORT_V_RES             (480)
 #endif
-#define LVGL_PORT_TICK_PERIOD_MS    (CONFIG_EXAMPLE_LVGL_PORT_TICK)
 
-/**
- * LVGL timer handle task related parameters, can be adjusted by users
- *
- */
+#define LVGL_PORT_TICK_PERIOD_MS    (CONFIG_EXAMPLE_LVGL_PORT_TICK)
 #define LVGL_PORT_TASK_MAX_DELAY_MS (CONFIG_EXAMPLE_LVGL_PORT_TASK_MAX_DELAY_MS)
 #define LVGL_PORT_TASK_MIN_DELAY_MS (CONFIG_EXAMPLE_LVGL_PORT_TASK_MIN_DELAY_MS)
 #define LVGL_PORT_TASK_STACK_SIZE   (CONFIG_EXAMPLE_LVGL_PORT_TASK_STACK_SIZE_KB * 1024)
 #define LVGL_PORT_TASK_PRIORITY     (CONFIG_EXAMPLE_LVGL_PORT_TASK_PRIORITY)
 #define LVGL_PORT_TASK_CORE         (CONFIG_EXAMPLE_LVGL_PORT_TASK_CORE)
 
-/**
- * LVGL buffer related parameters, can be adjusted by users:
- *  (These parameters will be useless if the avoid tearing function is enabled)
- *
- *  - Memory type for buffer allocation:
- *      - MALLOC_CAP_SPIRAM: Allocate LVGL buffer in PSRAM
- *      - MALLOC_CAP_INTERNAL: Allocate LVGL buffer in SRAM
- *      (The SRAM is faster than PSRAM, but the PSRAM has a larger capacity)
- *
- */
 #if CONFIG_EXAMPLE_LVGL_PORT_BUF_PSRAM
 #define LVGL_PORT_BUFFER_MALLOC_CAPS    (MALLOC_CAP_SPIRAM)
 #elif CONFIG_EXAMPLE_LVGL_PORT_BUF_INTERNAL
@@ -60,100 +42,25 @@ extern "C" {
 #endif
 #define LVGL_PORT_BUFFER_HEIGHT         (CONFIG_EXAMPLE_LVGL_PORT_BUF_HEIGHT)
 
-/**
- * Avoid tearing related configurations, controlled by menuconfig.
+/*
+ * Match the stable Waveshare example: two complete RGB frame buffers are used
+ * directly by LVGL and every update is rendered as a complete frame.
  */
-#define LVGL_PORT_AVOID_TEAR_ENABLE     (CONFIG_EXAMPLE_LVGL_PORT_AVOID_TEAR_ENABLE)
-#if LVGL_PORT_AVOID_TEAR_ENABLE
-#define LVGL_PORT_AVOID_TEAR_MODE       (CONFIG_EXAMPLE_LVGL_PORT_AVOID_TEAR_MODE)
-#define EXAMPLE_LVGL_PORT_ROTATION_DEGREE  (CONFIG_EXAMPLE_LVGL_PORT_ROTATION_DEGREE)
+#define LVGL_PORT_AVOID_TEAR_ENABLE       (1)
+#define LVGL_PORT_AVOID_TEAR_MODE         (1)
+#define EXAMPLE_LVGL_PORT_ROTATION_DEGREE (0)
 
-#if LVGL_PORT_AVOID_TEAR_MODE == 1
-#define LVGL_PORT_LCD_RGB_BUFFER_NUMS   (2)
-#define LVGL_PORT_FULL_REFRESH          (1)
-#define LVGL_PORT_DIRECT_MODE           (0)
-#elif LVGL_PORT_AVOID_TEAR_MODE == 2
-#define LVGL_PORT_LCD_RGB_BUFFER_NUMS   (3)
-#define LVGL_PORT_FULL_REFRESH          (1)
-#define LVGL_PORT_DIRECT_MODE           (0)
-#elif LVGL_PORT_AVOID_TEAR_MODE == 3
-#define LVGL_PORT_LCD_RGB_BUFFER_NUMS   (2)
-#define LVGL_PORT_FULL_REFRESH          (0)
-#define LVGL_PORT_DIRECT_MODE           (1)
-#endif
+#define LVGL_PORT_LCD_RGB_BUFFER_NUMS     (2)
+#define LVGL_PORT_FULL_REFRESH            (1)
+#define LVGL_PORT_DIRECT_MODE             (0)
+#define EXAMPLE_LVGL_PORT_ROTATION_0      (1)
+#define EXAMPLE_LVGL_PORT_ROTATION_90     (0)
+#define EXAMPLE_LVGL_PORT_ROTATION_180    (0)
+#define EXAMPLE_LVGL_PORT_ROTATION_270    (0)
 
-#if EXAMPLE_LVGL_PORT_ROTATION_DEGREE == 0
-#define EXAMPLE_LVGL_PORT_ROTATION_0    (1)
-#define EXAMPLE_LVGL_PORT_ROTATION_90   (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_180  (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_270  (0)
-#elif EXAMPLE_LVGL_PORT_ROTATION_DEGREE == 90
-#define EXAMPLE_LVGL_PORT_ROTATION_0    (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_90   (1)
-#define EXAMPLE_LVGL_PORT_ROTATION_180  (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_270  (0)
-#elif EXAMPLE_LVGL_PORT_ROTATION_DEGREE == 180
-#define EXAMPLE_LVGL_PORT_ROTATION_0    (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_90   (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_180  (1)
-#define EXAMPLE_LVGL_PORT_ROTATION_270  (0)
-#elif EXAMPLE_LVGL_PORT_ROTATION_DEGREE == 270
-#define EXAMPLE_LVGL_PORT_ROTATION_0    (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_90   (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_180  (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_270  (1)
-#endif
-
-#if EXAMPLE_LVGL_PORT_ROTATION_DEGREE != 0
-#undef LVGL_PORT_LCD_RGB_BUFFER_NUMS
-#define LVGL_PORT_LCD_RGB_BUFFER_NUMS   (3)
-#endif
-#else
-#define LVGL_PORT_LCD_RGB_BUFFER_NUMS   (1)
-#define LVGL_PORT_FULL_REFRESH          (0)
-#define LVGL_PORT_DIRECT_MODE           (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_0    (1)
-#define EXAMPLE_LVGL_PORT_ROTATION_90   (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_180  (0)
-#define EXAMPLE_LVGL_PORT_ROTATION_270  (0)
-#endif
-
-/**
- * @brief Initialize LVGL port
- *
- * @param[in] lcd_handle: LCD panel handle
- * @param[in] tp_handle: Touch panel handle
- *
- * @return
- *      - ESP_OK: Success
- *      - ESP_ERR_INVALID_ARG: Invalid argument
- *      - Others: Fail
- */
 esp_err_t lvgl_port_init(esp_lcd_panel_handle_t lcd_handle, esp_lcd_touch_handle_t tp_handle);
-
-/**
- * @brief Take LVGL mutex
- *
- * @param[in] timeout_ms: Timeout in [ms]. 0 will block indefinitely.
- *
- * @return
- *      - true:  Mutex was taken
- *      - false: Mutex was NOT taken
- */
 bool lvgl_port_lock(int timeout_ms);
-
-/**
- * @brief Give LVGL mutex
- */
 void lvgl_port_unlock(void);
-
-/**
- * @brief Notifies the LVGL task when the transmission of the RGB frame buffer is completed.
- *
- * @return
- *      - true:  The tasks need to be re-scheduled
- *      - false: The tasks don't need to be re-scheduled
- */
 bool lvgl_port_notify_rgb_vsync(void);
 
 #ifdef __cplusplus
