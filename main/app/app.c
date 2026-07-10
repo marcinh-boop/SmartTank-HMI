4726abef2c685f6adefd94d9352802656a7e1d4f
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 20:10:32 +0200

    Restore SmartTank dashboard on stable display driver

--- main/app/app.c
+++ main/app/app.c
@@ -1,11 +1,11 @@
 #include "app.h"
 #include "lvgl_port.h"
-#include "screen_display_test.h"
+#include "screen_dashboard.h"
 
 void app_start(void)
 {
     if (lvgl_port_lock(-1)) {
-        screen_display_test_create();
+        screen_dashboard_create();
         lvgl_port_unlock();
     }
 }
