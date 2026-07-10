209db4d266bba0296dcb472ba6389fc2329de81a
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 23:12:42 +0200

    Extend clock service with system time sources

--- main/services/clock_service.h
+++ main/services/clock_service.h
@@ -2,16 +2,28 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <time.h>
 
 #include "esp_err.h"
 #include "rtc_pcf85063.h"
+
+typedef enum {
+    CLOCK_TIME_SOURCE_NONE = 0,
+    CLOCK_TIME_SOURCE_RTC,
+    CLOCK_TIME_SOURCE_NTP,
+    CLOCK_TIME_SOURCE_MANUAL,
+} clock_time_source_t;
 
 typedef struct {
     bool started;
     bool rtc_present;
     bool time_valid;
     bool oscillator_stopped;
+    bool system_time_valid;
+    clock_time_source_t source;
     rtc_datetime_t datetime;
+    rtc_datetime_t local_datetime;
+    time_t last_ntp_sync_epoch;
     esp_err_t last_error;
     uint32_t successful_reads;
     uint32_t read_errors;
@@ -20,3 +32,5 @@ typedef struct {
 esp_err_t clock_service_start(void);
 void clock_service_get_snapshot(clock_service_snapshot_t *snapshot);
 esp_err_t clock_service_set_datetime(const rtc_datetime_t *datetime);
+esp_err_t clock_service_accept_ntp_time(time_t epoch);
+const char *clock_service_source_name(clock_time_source_t source);
