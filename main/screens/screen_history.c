598a977b4e401ad79346dfccfeb0ebada4bcf577
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-10 16:59:55 +0200

    Cache history screen for instant navigation

--- main/screens/screen_history.c
+++ main/screens/screen_history.c
@@ -4,16 +4,12 @@
 #include "theme.h"
 #include "lvgl.h"
 
-static void open_dashboard_async(void *user_data)
-{
-    (void)user_data;
-    screen_dashboard_create();
-}
+static lv_obj_t *s_history_screen = NULL;
 
 static bool history_nav_change(bottom_nav_page_t page)
 {
     if (page == NAV_DASHBOARD) {
-        lv_async_call(open_dashboard_async, NULL);
+        screen_dashboard_create();
         return true;
     }
 
@@ -26,14 +22,12 @@ static lv_obj_t *create_panel(lv_obj_t *parent, int x, int width)
     lv_obj_set_size(panel, width, 330);
     lv_obj_align(panel, LV_ALIGN_TOP_LEFT, x, 68);
     lv_obj_clear_flag(panel, LV_OBJ_FLAG_SCROLLABLE);
-
     lv_obj_set_style_bg_color(panel, ST_COLOR_PANEL, LV_PART_MAIN);
     lv_obj_set_style_bg_opa(panel, LV_OPA_COVER, LV_PART_MAIN);
     lv_obj_set_style_radius(panel, 14, LV_PART_MAIN);
     lv_obj_set_style_border_width(panel, 1, LV_PART_MAIN);
     lv_obj_set_style_border_color(panel, lv_color_hex(0x24384A), LV_PART_MAIN);
     lv_obj_set_style_pad_all(panel, 14, LV_PART_MAIN);
-
     return panel;
 }
 
@@ -43,7 +37,6 @@ static void create_top_bar(lv_obj_t *screen)
     lv_obj_set_size(top, 760, 50);
     lv_obj_align(top, LV_ALIGN_TOP_MID, 0, 8);
     lv_obj_clear_flag(top, LV_OBJ_FLAG_SCROLLABLE);
-
     lv_obj_set_style_bg_color(top, lv_color_hex(0x08131F), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(top, LV_OPA_COVER, LV_PART_MAIN);
     lv_obj_set_style_radius(top, 10, LV_PART_MAIN);
@@ -74,12 +67,10 @@ static void create_bottom_bar(lv_obj_t *screen)
     lv_obj_set_size(bottom, 760, 50);
     lv_obj_align(bottom, LV_ALIGN_BOTTOM_MID, 0, -8);
     lv_obj_clear_flag(bottom, LV_OBJ_FLAG_SCROLLABLE);
-
     lv_obj_set_style_bg_color(bottom, lv_color_hex(0x08131F), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(bottom, LV_OPA_COVER, LV_PART_MAIN);
     lv_obj_set_style_radius(bottom, 10, LV_PART_MAIN);
     lv_obj_set_style_border_width(bottom, 0, LV_PART_MAIN);
-
     bottom_nav_create(bottom, NAV_HISTORY, history_nav_change);
 }
 
@@ -99,24 +90,17 @@ static lv_obj_t *create_chart(
     lv_obj_t *chart = lv_chart_create(parent);
     lv_obj_set_size(chart, 330, 235);
     lv_obj_align(chart, LV_ALIGN_BOTTOM_MID, 0, -4);
-
     lv_chart_set_type(chart, LV_CHART_TYPE_LINE);
     lv_chart_set_range(chart, LV_CHART_AXIS_PRIMARY_Y, 0, 100);
     lv_chart_set_point_count(chart, count);
     lv_chart_set_div_line_count(chart, 5, 6);
-
     lv_obj_set_style_bg_color(chart, lv_color_hex(0x09131D), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(chart, LV_OPA_COVER, LV_PART_MAIN);
     lv_obj_set_style_border_width(chart, 0, LV_PART_MAIN);
     lv_obj_set_style_line_color(chart, lv_color_hex(0x23384A), LV_PART_MAIN);
     lv_obj_set_style_line_width(chart, 1, LV_PART_MAIN);
 
-    lv_chart_series_t *series = lv_chart_add_series(
-        chart,
-        color,
-        LV_CHART_AXIS_PRIMARY_Y
-    );
-
+    lv_chart_series_t *series = lv_chart_add_series(chart, color, LV_CHART_AXIS_PRIMARY_Y);
     for (uint32_t i = 0; i < count; i++) {
         lv_chart_set_next_value(chart, series, values[i]);
     }
@@ -130,7 +114,7 @@ static lv_obj_t *create_chart(
     return chart;
 }
 
-void screen_history_create(void)
+static void build_history_screen(void)
 {
     static const lv_coord_t tank_values[24] = {
         54, 55, 55, 56, 57, 58, 59, 60,
@@ -144,31 +128,27 @@ void screen_history_create(void)
         71, 70, 70, 69, 70, 70, 70, 70
     };
 
-    lv_obj_t *screen = lv_scr_act();
-    lv_obj_clean(screen);
-    lv_obj_clear_flag(screen, LV_OBJ_FLAG_SCROLLABLE);
-    lv_obj_set_style_bg_color(screen, ST_COLOR_BG, LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, LV_PART_MAIN);
+    s_history_screen = lv_obj_create(NULL);
+    lv_obj_clear_flag(s_history_screen, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_style_bg_color(s_history_screen, ST_COLOR_BG, LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(s_history_screen, LV_OPA_COVER, LV_PART_MAIN);
 
-    create_top_bar(screen);
+    create_top_bar(s_history_screen);
 
-    lv_obj_t *tank_panel = create_panel(screen, 20, 370);
-    create_chart(
-        tank_panel,
-        "SZAMBO  72%",
-        lv_color_hex(0x39D12F),
-        tank_values,
-        24
-    );
+    lv_obj_t *tank_panel = create_panel(s_history_screen, 20, 370);
+    create_chart(tank_panel, "SZAMBO  72%", lv_color_hex(0x39D12F), tank_values, 24);
 
-    lv_obj_t *well_panel = create_panel(screen, 410, 370);
-    create_chart(
-        well_panel,
-        "STUDNIA  2.81 m",
-        lv_color_hex(0x2EA8FF),
-        well_values,
-        24
-    );
+    lv_obj_t *well_panel = create_panel(s_history_screen, 410, 370);
+    create_chart(well_panel, "STUDNIA  2.81 m", lv_color_hex(0x2EA8FF), well_values, 24);
 
-    create_bottom_bar(screen);
+    create_bottom_bar(s_history_screen);
+}
+
+void screen_history_create(void)
+{
+    if (s_history_screen == NULL) {
+        build_history_screen();
+    }
+
+    lv_scr_load(s_history_screen);
 }
