41d9bd4da800155cc3b3f5c7501d48757c88fa5e
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-11 08:55:53 +0200

    Dodaj dynamiczną ikonę i status aktualizacji pogody

--- main/widgets/weather_widget.h
+++ main/widgets/weather_widget.h
@@ -8,10 +8,20 @@ typedef struct {
 
     lv_obj_t *temperature_label;
     lv_obj_t *description_label;
+    lv_obj_t *update_status_label;
 
     lv_obj_t *rain_value;
     lv_obj_t *wind_value;
     lv_obj_t *humidity_value;
+
+    lv_obj_t *icon_root;
+    lv_obj_t *icon_clear;
+    lv_obj_t *icon_partly_cloudy;
+    lv_obj_t *icon_cloudy;
+    lv_obj_t *icon_fog;
+    lv_obj_t *icon_rain;
+    lv_obj_t *icon_snow;
+    lv_obj_t *icon_storm;
 
     lv_obj_t *forecast_day[WEATHER_FORECAST_DAYS];
     lv_obj_t *forecast_temp[WEATHER_FORECAST_DAYS];
