1c6fe1d378633107b0e97216a598d1ef7ce2b0fc
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-11 13:06:35 +0200

    Dodaj interfejs kalibracji studni

---
+++ main/screens/screen_well_calibration.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "app_model.h"
+#include "lvgl.h"
+#include "well_settings.h"
+
+typedef void (*screen_well_calibration_back_cb_t)(void);
+typedef void (*screen_well_calibration_save_cb_t)(const well_settings_t *settings);
+
+lv_obj_t *screen_well_calibration_create(
+    lv_obj_t *parent,
+    screen_well_calibration_back_cb_t back_cb,
+    screen_well_calibration_save_cb_t save_cb
+);
+
+void screen_well_calibration_begin(const smarttank_state_t *state);
+void screen_well_calibration_update_live(const smarttank_state_t *state);
