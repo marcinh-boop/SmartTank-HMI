496f4ee2b0c3c281e98df50cd57daf30ecbfc600
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-11 13:11:23 +0200

    Stosuj progi kalibracji na kafelku studni

--- main/widgets/well_widget.c
+++ main/widgets/well_widget.c
@@ -1,6 +1,7 @@
 #include "well_widget.h"
 #include "widget_common.h"
 #include "theme.h"
+#include "well_settings.h"
 
 #include <stdio.h>
 
@@ -18,14 +19,7 @@ well_widget_t well_widget_create(lv_obj_t *parent)
 
     lv_obj_clear_flag(parent, LV_OBJ_FLAG_SCROLLABLE);
 
-    widget_label_create(
-        parent,
-        "STUDNIA",
-        WELL_BLUE,
-        LV_ALIGN_TOP_LEFT,
-        0,
-        0
-    );
+    widget_label_create(parent, "STUDNIA", WELL_BLUE, LV_ALIGN_TOP_LEFT, 0, 0);
 
     widget.depth_label = widget_label_create(
         parent,
@@ -39,18 +33,14 @@ well_widget_t well_widget_create(lv_obj_t *parent)
     widget.arc = lv_arc_create(parent);
     lv_obj_set_size(widget.arc, 122, 122);
     lv_obj_align(widget.arc, LV_ALIGN_CENTER, -36, -27);
-
     lv_arc_set_range(widget.arc, 0, 100);
     lv_arc_set_value(widget.arc, 70);
     lv_arc_set_bg_angles(widget.arc, 135, 45);
-
     lv_obj_remove_style(widget.arc, NULL, LV_PART_KNOB);
     lv_obj_clear_flag(widget.arc, LV_OBJ_FLAG_CLICKABLE);
-
     lv_obj_set_style_arc_width(widget.arc, 9, LV_PART_MAIN);
     lv_obj_set_style_arc_rounded(widget.arc, true, LV_PART_MAIN);
     lv_obj_set_style_arc_color(widget.arc, WELL_DARK_BLUE, LV_PART_MAIN);
-
     lv_obj_set_style_arc_width(widget.arc, 9, LV_PART_INDICATOR);
     lv_obj_set_style_arc_rounded(widget.arc, true, LV_PART_INDICATOR);
     lv_obj_set_style_arc_color(widget.arc, WELL_BLUE, LV_PART_INDICATOR);
@@ -76,22 +66,10 @@ well_widget_t well_widget_create(lv_obj_t *parent)
     widget.vertical_bar = lv_bar_create(parent);
     lv_obj_set_size(widget.vertical_bar, 18, 105);
     lv_obj_align(widget.vertical_bar, LV_ALIGN_RIGHT_MID, -8, -27);
-
     lv_bar_set_range(widget.vertical_bar, 0, 100);
     lv_bar_set_value(widget.vertical_bar, 70, LV_ANIM_OFF);
-
-    lv_obj_set_style_bg_color(
-        widget.vertical_bar,
-        WELL_DARK_BLUE,
-        LV_PART_MAIN
-    );
-
-    lv_obj_set_style_bg_color(
-        widget.vertical_bar,
-        WELL_BLUE,
-        LV_PART_INDICATOR
-    );
-
+    lv_obj_set_style_bg_color(widget.vertical_bar, WELL_DARK_BLUE, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(widget.vertical_bar, WELL_BLUE, LV_PART_INDICATOR);
     lv_obj_set_style_radius(widget.vertical_bar, 5, LV_PART_MAIN);
     lv_obj_set_style_radius(widget.vertical_bar, 5, LV_PART_INDICATOR);
     lv_obj_set_style_border_width(widget.vertical_bar, 1, LV_PART_MAIN);
@@ -109,7 +87,6 @@ well_widget_t well_widget_create(lv_obj_t *parent)
         -30,
         -75
     );
-
     widget.scale_mid_label = widget_label_create(
         parent,
         "2.00 m",
@@ -118,7 +95,6 @@ well_widget_t well_widget_create(lv_obj_t *parent)
         -30,
         -2
     );
-
     widget.scale_bottom_label = widget_label_create(
         parent,
         "0.00 m",
@@ -135,12 +111,7 @@ well_widget_t well_widget_create(lv_obj_t *parent)
         WELL_BORDER,
         -78
     );
-
-    lv_obj_set_style_text_color(
-        widget.reserve_value,
-        WELL_BLUE,
-        LV_PART_MAIN
-    );
+    lv_obj_set_style_text_color(widget.reserve_value, WELL_BLUE, LV_PART_MAIN);
 
     widget.consumption_value = widget_info_row_create(
         parent,
@@ -149,12 +120,7 @@ well_widget_t well_widget_create(lv_obj_t *parent)
         WELL_BORDER,
         -50
     );
-
-    lv_obj_set_style_text_color(
-        widget.consumption_value,
-        WELL_BLUE,
-        LV_PART_MAIN
-    );
+    lv_obj_set_style_text_color(widget.consumption_value, WELL_BLUE, LV_PART_MAIN);
 
     widget.regeneration_value = widget_info_row_create(
         parent,
@@ -163,12 +129,7 @@ well_widget_t well_widget_create(lv_obj_t *parent)
         WELL_BORDER,
         -22
     );
-
-    lv_obj_set_style_text_color(
-        widget.regeneration_value,
-        WELL_GREEN,
-        LV_PART_MAIN
-    );
+    lv_obj_set_style_text_color(widget.regeneration_value, WELL_GREEN, LV_PART_MAIN);
 
     widget.status_label = widget_label_create(
         parent,
@@ -192,22 +153,19 @@ void well_widget_set_data(
     }
 
     int percent = (int)((water_column_m / well_depth_m) * 100.0f);
+    if (percent < 0) percent = 0;
+    if (percent > 100) percent = 100;
 
-    if (percent < 0) {
-        percent = 0;
-    }
-
-    if (percent > 100) {
-        percent = 100;
-    }
+    well_settings_t settings;
+    well_settings_get(&settings);
 
     lv_color_t status_color = WELL_BLUE;
     const char *status_text = "Poziom OK";
 
-    if (percent <= 15) {
+    if (percent <= settings.critical_percent) {
         status_color = WELL_RED;
         status_text = "ALARM - niski poziom";
-    } else if (percent <= 30) {
+    } else if (percent <= settings.warning_percent) {
         status_color = WELL_YELLOW;
         status_text = "Niski poziom";
     }
@@ -230,24 +188,8 @@ void well_widget_set_data(
 
     lv_arc_set_value(widget->arc, percent);
     lv_bar_set_value(widget->vertical_bar, percent, LV_ANIM_ON);
-
-    lv_obj_set_style_arc_color(
-        widget->arc,
-        status_color,
-        LV_PART_INDICATOR
-    );
-
-    lv_obj_set_style_bg_color(
-        widget->vertical_bar,
-        status_color,
-        LV_PART_INDICATOR
-    );
-
+    lv_obj_set_style_arc_color(widget->arc, status_color, LV_PART_INDICATOR);
+    lv_obj_set_style_bg_color(widget->vertical_bar, status_color, LV_PART_INDICATOR);
     lv_label_set_text(widget->status_label, status_text);
-
-    lv_obj_set_style_text_color(
-        widget->status_label,
-        status_color,
-        LV_PART_MAIN
-    );
+    lv_obj_set_style_text_color(widget->status_label, status_color, LV_PART_MAIN);
 }
