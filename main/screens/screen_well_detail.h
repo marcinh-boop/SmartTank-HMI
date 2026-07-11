785cfdb500aba6ea7f7023c4a72b62343ab7cfa2
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-11 12:56:10 +0200

    Dodaj interfejs szczegółów studni

---
+++ main/screens/screen_well_detail.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "app_model.h"
+#include "lvgl.h"
+
+typedef void (*screen_well_detail_back_cb_t)(void);
+
+lv_obj_t *screen_well_detail_create(
+    lv_obj_t *parent,
+    screen_well_detail_back_cb_t back_cb
+);
+
+void screen_well_detail_update(const smarttank_state_t *state);
