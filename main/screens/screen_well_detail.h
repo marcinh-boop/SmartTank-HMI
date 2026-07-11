841a3cf85a98154cc1417c1a59dfee5e52150eb0
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-11 13:07:34 +0200

    Dodaj przejście do kalibracji studni

--- main/screens/screen_well_detail.h
+++ main/screens/screen_well_detail.h
@@ -4,10 +4,12 @@
 #include "lvgl.h"
 
 typedef void (*screen_well_detail_back_cb_t)(void);
+typedef void (*screen_well_detail_calibration_cb_t)(void);
 
 lv_obj_t *screen_well_detail_create(
     lv_obj_t *parent,
-    screen_well_detail_back_cb_t back_cb
+    screen_well_detail_back_cb_t back_cb,
+    screen_well_detail_calibration_cb_t calibration_cb
 );
 
 void screen_well_detail_update(const smarttank_state_t *state);
