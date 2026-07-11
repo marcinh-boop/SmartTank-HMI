d091f3ed0b284f8972f872c6c719c2c0f293915b
marcinh-boop <marcin.hoinca@gmail.com>
2026-07-11 07:40:26 +0200

    Dodaj interfejs zapisu lokalizacji pogody

---
+++ main/services/weather_location_storage.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "esp_err.h"
+#include "weather_location.h"
+
+esp_err_t weather_location_storage_load(weather_location_t *location);
+esp_err_t weather_location_storage_save(const weather_location_t *location);
+esp_err_t weather_location_storage_clear(void);
